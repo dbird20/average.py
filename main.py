2b0e6949092303113f2691ea8111a978be2c5208
dillonbirden <dbirden5@gmail.com>
2026-01-12 22:59:56 -0600

    This is starting to get fun.

---
+++ main.py
@@ -0,0 +1,25 @@
+# Author: Dillon Birden
+# GitHub username: dbird20
+# Date: 1/12/2026
+# Description: Write a program that asks the user for five numbers and then prints out the average of those numbers.
+
+
+
+
+
+
+print("Please enter five numbers.")
+
+
+num_1 = float(input(""))
+num_2 = float(input(""))
+num_3 = float(input(""))
+num_4 = float(input(""))
+num_5 = float(input(""))
+
+
+my_sum = num_1 + num_2 + num_3 + num_4 + num_5
+
+print("The average of those numbers is:", my_sum / 5)
+
+
